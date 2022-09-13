aca2bb040ebaa777a0cdad1d1a4e2626fa22855b
Melissa OUHADDAD <mo683247@preseed.iem>
2022-09-13 11:26:16 +0200

    ajout Cerise

---
+++ Cerise.java
@@ -0,0 +1,68 @@
+/**
+ *
+ * @author roudet
+ */
+public class Cerise {
+    private double prix;
+    private String origine;
+	
+    public Cerise() 
+    {
+        this.prix =1.5  ;  //prix en euros
+        this.origine="Italie";
+    }
+    
+    public Cerise(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Italie";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Cerise de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Cerise or = (Cerise) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        Cerise cr1 = new Cerise(-4, "france");
+	System.out.println("premier test Cerise");
+   }
+    
+}
